57c8309b62cc4023de13eda7337e8b4058acfa69
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-13 00:01:42 +0530

    [Chetan]Add. Special Rates for Reward customer.

---
+++ HotelReservationSystem/CustomerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    //class CustomerType
+    //{
+        public enum CustomerType
+        {
+            REGULAR, 
+            REWARD
+        }
+    //}
+}
