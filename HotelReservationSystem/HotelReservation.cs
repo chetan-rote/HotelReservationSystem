bef3a54191fef8de0d7735f6893fe4906c5e347f
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-12 18:04:27 +0530

    [Chetan]Add. Add hotels and their rates.

---
+++ HotelReservationSystem/HotelReservation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    class HotelReservation
+    {
+        public static Dictionary<string, HotelDetails> onlineHotelRecords = new Dictionary<string, HotelDetails>();
+
+        /// <summary>
+        /// UC1
+        /// Add Hotels.
+        /// </summary>
+        /// <param name="hotelName">Name of the hotel.</param>
+        /// <param name="ratePerDay">The rate per day.</param>
+        public static void AddHotelRecords(string hotelName, int ratePerDay)
+        {
+            if (onlineHotelRecords.ContainsKey(hotelName))
+            {
+                Console.WriteLine("Hotel already exists.");
+            }
+            else
+            {
+                HotelDetails newHotel = new HotelDetails(hotelName, ratePerDay);
+                onlineHotelRecords.Add(hotelName, newHotel);
+            }
+        }
+        /// <summary>
+        /// Displays the Hotels in dictionary.
+        /// </summary>
+        public static void DisplayRecordsInDictionary()
+        {
+            foreach (var records in onlineHotelRecords)
+            {
+                Console.WriteLine($"Hotel Name = {records.Key}, Rate Per Day = {records.Value.ratePerDay}\n");
+            }
+        }
+    }
+}
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿
+/******************************************************************************
+ *  Purpose: Hotel Rservation System to view book the hotel room.
+ *
+ *
+ *  @author  Chetan Rote
+ *  @version 1.0
+ *  @since   11-11-2020
+ ******************************************************************************/
+using System;
 
 namespace HotelReservationSystem
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome to Hotel Reservation System\n");
+            /// UC1 --Adding the hotels name and rates.
+            HotelReservation.AddHotelRecords("Lakewood", 110);
+            HotelReservation.AddHotelRecords("Bridgewood", 150);
+            HotelReservation.AddHotelRecords("Ridgewood", 220);
+            /// Display the record in the hotel record dictionary.
+            HotelReservation.DisplayRecordsInDictionary();
+            Console.ReadKey();
         }
     }
 }
